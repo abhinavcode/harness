1f188e4a3a751a0ad9f9901a090934a74ccc65b2
Shubham Singla <shubham.singla@harness.io>
2025-11-20 14:51:44 +0530

    fized migrtions

--- app/store/database/migrate/postgres/0145_add_soft_delete_columns.down.sql
+++ app/store/database/migrate/postgres/0145_add_soft_delete_columns.down.sql
@@ -1,17 +1,17 @@
 -- Remove from artifacts table
-DROP INDEX IF EXISTS idx_artifact_deleted_at;
+DROP INDEX IF EXISTS idx_artifacts_deleted_at;
 ALTER TABLE artifacts 
     DROP COLUMN IF EXISTS artifact_deleted_at,
     DROP COLUMN IF EXISTS artifact_deleted_by;
 
 -- Remove from images table
-DROP INDEX IF EXISTS idx_image_deleted_at;
+DROP INDEX IF EXISTS idx_images_deleted_at;
 ALTER TABLE images 
     DROP COLUMN IF EXISTS image_deleted_at,
     DROP COLUMN IF EXISTS image_deleted_by;
 
 -- Remove from registries table
-DROP INDEX IF EXISTS idx_registry_deleted_at;
+DROP INDEX IF EXISTS idx_registries_deleted_at;
 ALTER TABLE registries 
     DROP COLUMN IF EXISTS registry_deleted_at,
     DROP COLUMN IF EXISTS registry_deleted_by;
