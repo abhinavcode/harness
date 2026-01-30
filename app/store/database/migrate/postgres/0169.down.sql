544004c57a0ae9a48185cbaa3f5b39e051adc8bc
Shubham Singla <shubham.singla@harness.io>
2026-01-30 13:09:09 +0530

    Adds index on jobs table

---
+++ app/store/database/migrate/postgres/0169.down.sql
@@ -0,0 +1,1 @@
+DROP INDEX IF EXISTS jobs_scheduled_priority;
---
+++ app/store/database/migrate/sqlite/0169.up.sql
@@ -0,0 +1,4 @@
+-- Covers: WHERE job_state = 'scheduled' AND job_scheduled <= ? ORDER BY job_priority DESC, job_scheduled ASC
+CREATE INDEX jobs_scheduled_priority
+	ON jobs(job_priority DESC, job_scheduled ASC)
+	WHERE job_state = 'scheduled';
