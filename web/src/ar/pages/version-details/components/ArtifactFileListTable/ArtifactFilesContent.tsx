db4b374409f06ef160958cc6d3c1b43da624d4cf
shivang <shivang.kaushal@harness.io>
2026-02-10 16:10:43 +0530

    feat: [2732]: search bar for artifact version

--- web/src/ar/pages/version-details/components/ArtifactFileListTable/ArtifactFilesContent.tsx
+++ web/src/ar/pages/version-details/components/ArtifactFileListTable/ArtifactFilesContent.tsx
@@ -14,27 +14,68 @@
  * limitations under the License.
  */
 
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
+import { Container, Layout, ExpandingSearchInput, ExpandingSearchInputHandle, PageError } from '@harnessio/uicore'
+import { Icon } from '@harnessio/icons'
 
 import { DEFAULT_PAGE_INDEX } from '@ar/constants'
+import { useStrings } from '@ar/frameworks/strings'
 import { VersionFilesContext } from '@ar/pages/version-details/context/VersionFilesProvider'
 import ArtifactFileListTable from '@ar/pages/version-details/components/ArtifactFileListTable/ArtifactFileListTable'
+
+import css from './ArtifactFilesContent.module.scss'
 
 interface ArtifactFilesContentProps {
   minimal?: boolean
 }
 export default function ArtifactFilesContent(props: ArtifactFilesContentProps): JSX.Element {
   const { minimal } = props
-  const { data, updateQueryParams, sort } = useContext(VersionFilesContext)
+  const { getString } = useStrings()
+  const searchRef = useRef<ExpandingSearchInputHandle>({} as ExpandingSearchInputHandle)
+  const { data, loading, error, refetch, updateQueryParams, sort, queryParams } = useContext(VersionFilesContext)
+
   return (
-    <ArtifactFileListTable
-      data={data}
-      gotoPage={pageNumber => updateQueryParams({ page: pageNumber })}
-      setSortBy={sortArr => {
-        updateQueryParams({ sort: sortArr, page: DEFAULT_PAGE_INDEX })
-      }}
-      sortBy={sort}
-      minimal={minimal}
-    />
+    <Layout.Vertical spacing="medium">
+      {!minimal && (
+        <ExpandingSearchInput
+          alwaysExpanded
+          width={500}
+          placeholder={getString('search')}
+          onChange={text => {
+            updateQueryParams({ searchTerm: text || undefined, page: DEFAULT_PAGE_INDEX })
+          }}
+          defaultValue={queryParams?.searchTerm || ''}
+          ref={searchRef}
+        />
+      )}
+      <Container className={css.tableContainer}>
+        {loading ? (
+          <Container flex={{ justifyContent: 'center', alignItems: 'center' }} padding="xlarge">
+            <Icon name="steps-spinner" size={32} />
+          </Container>
+        ) : error && !loading ? (
+          <PageError
+            message={
+              error instanceof Error
+                ? error.message
+                : typeof error === 'object' && error !== null && 'message' in error
+                ? String((error as { message?: unknown }).message)
+                : String(error)
+            }
+            onClick={() => refetch()}
+          />
+        ) : data ? (
+          <ArtifactFileListTable
+            data={data}
+            gotoPage={pageNumber => updateQueryParams({ page: pageNumber })}
+            setSortBy={sortArr => {
+              updateQueryParams({ sort: sortArr, page: DEFAULT_PAGE_INDEX })
+            }}
+            sortBy={sort}
+            minimal={minimal}
+          />
+        ) : null}
+      </Container>
+    </Layout.Vertical>
   )
 }
