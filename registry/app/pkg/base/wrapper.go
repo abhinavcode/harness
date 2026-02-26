c22b9e2268e350365cd1d6c3da3166178c97fd3f
Pragyesh Mishra <pragyesh.mishra@harness.io>
2026-02-26 20:59:15 +0530

    [feat]: [AH-2903]: add support for upload/download for raw files

--- registry/app/pkg/base/wrapper.go
+++ registry/app/pkg/base/wrapper.go
@@ -201,8 +201,16 @@ func factory(key string) pkg.Artifact {
 	return TypeRegistry[key]
 }
 
+func Factory(key string) pkg.Artifact {
+	return factory(key)
+}
+
 func getFactoryKey(packageType artifact.PackageType, registryType artifact.RegistryType) string {
 	return string(packageType) + ":" + string(registryType)
+}
+
+func GetFactoryKey(packageType artifact.PackageType, registryType artifact.RegistryType) string {
+	return getFactoryKey(packageType, registryType)
 }
 
 func GetArtifactRegistry(registry registrytypes.Registry) pkg.Artifact {
