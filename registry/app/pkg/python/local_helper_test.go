3821da77d2263dab5bb7e286329dba4db77210e8
Pragyesh Mishra <pragyesh.mishra@harness.io>
2026-02-26 22:35:30 +0530

    [feat]: [AH-2903]: add support for upload/download for raw files

--- registry/app/pkg/python/local_helper_test.go
+++ registry/app/pkg/python/local_helper_test.go
@@ -145,11 +145,6 @@ func (m *MockLocalBase) Exists(ctx context.Context, info pkg.ArtifactInfo, path 
 	return args.Bool(0)
 }
 
-func (m *MockLocalBase) ExistsByFilePath(ctx context.Context, registryID int64, filePath string) (bool, error) {
-	args := m.Called(ctx, registryID, filePath)
-	return args.Bool(0), args.Error(1)
-}
-
 func (m *MockLocalBase) Download(ctx context.Context, info pkg.ArtifactInfo, version, filename string) (
 	*commons.ResponseHeaders, *storage.FileReader, string, error,
 ) {
